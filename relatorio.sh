b358bed41d03474a7bc5c306be4415f7152290af
dario-santos <dariovfsantos@gmail.com>
2019-04-06 03:00:24 +0100

    Ligeiras alterações nos relatórios
    Arranjo um erro em que o grep estava a tentar aceder a ficheiros que não existiam.
    Pequenas alterações de sintaxe nos echo.

--- relatorio.sh
+++ relatorio.sh
@@ -4,27 +4,27 @@ function relatorio_aluno {
 	read var
 	echo $var
 
-	linha=`grep $var aluno`
+	linha=`grep $var ./BD/Aluno.txt`
 	echo $linha
 	
 	IFS=":" read -r idAluno nome idUniDest idUniOrig idRespDest idRespOrig <<< "$linha"
 	echo "$idAluno $nome $idUniDest"
 
-	linha2=`grep $idUniDest UniversdidadeDestino`
+	linha2=`grep $idUniDest ./BD/UniDest.txt`
 	IFS=":" read -r var1 nomeUniDestino <<< "$linha2"
 
-	linha3=`grep $idUniOrig UniversdidadeOrigem`
+	linha3=`grep $idUniOrig ./BD/UniOrig.txt`
 	IFS=":" read -r var2 nomeUniOrigem <<< "$linha3"
 
-	linha4=`grep $idRespDest ResponsavelDestino`
+	linha4=`grep $idRespDest ./BD/RespDest.txt`
 	IFS=":" read -r var3 nomeRespDestino <<< "$linha4"
 
-	linha5=`grep $idRespDest ResponsavelOrigem`
+	linha5=`grep $idRespDest ./BD/RespOrig.txt`
 	IFS=":" read -r var3 nomeRespOrigem <<< "$linha5"
 
-
-	echo 'O aluno' $nome 'com o id' $idAluno 'vai para a' $nomeUniDestino 'vindo da' $nomeUniOrigem '. O antigo responsavel era:' $nomeRespOrigem 'e o novo responsavel é:' $nomeRespDestino
-
+	echo "O aluno ${nome} com o id ${idAluno}"
+	echo "vai para a ${nomeUniDestino} vindo da ${nomeUniOrigem}."
+	echo "O responsavel de origem é ${nomeRespOrigem} e o responsavel de destino é ${nomeRespDestino}."
      
 }
 
@@ -32,11 +32,11 @@ function relatorio_disciplinascomalunos {
 	echo -e "\n--Relatório da Disciplina--\n"
 	echo -n "Introduza o id da Disciplina: "
 	read var
-	linha5=`grep $var Disciplina`
+	linha5=`grep $var ./BD/Disciplina.txt`
 	IFS=":" read -r idDisciplina var1 var2 var3 idRespDest <<< "$linha5"
 
 	echo "A disciplina tem os seguintes alunos"
-	grep $idRespDest aluno | awk -F ':' '{print $2}'
+	grep $idRespDest "./BD/Aluno.txt" | awk -F ':' '{print $2}'
 	
 }
     
@@ -47,13 +47,13 @@ function relatorio_universidade {
 	read var
 	
 	echo "A universidade de origem tem os seguintes alunos:"		
-	grep $var aluno | awk -F ':' '{print $2}'
+	grep $var ".BD/Aluno.txt" | awk -F ':' '{print $2}'
 	
 	echo -n "Introduza o id da Universidade de Destino: "
 	read var
 	
 	echo "A universidade de destino tem os seguintes alunos:"		
-	grep $var aluno | awk -F ':' '{print $2}'
+	grep $var ".BD/Aluno.txt" | awk -F ':' '{print $2}'
 	
 }
 
@@ -61,15 +61,17 @@ function relatorio_disciplina {
     	echo -e "\n--Relatório da Disciplina--\n"
 	echo -n "Introduza o id da disciplina: "	
 	read var
-	linha=`grep $var Disciplina`
+	linha=`grep $var ./BD/Disciplina.txt`
 	echo $linha
 	IFS=":" read -r idDisciplina nome ano semestre idRespDest <<< "$linha"
 	
-	linha4=`grep $idRespDest ResponsavelDestino`
+	linha4=`grep $idRespDest ./BD/RespDest.txt`
 	IFS=":" read -r var3 nomeRespDestino <<< "$linha4"
 	
 	
 	
 	echo 'A disciplina denominada' $nome 'foi criada no ano de' $ano 'e é lecionada no' $semestre 'semestre e o responsavel é o professor' $nomeRespDestino
-   
 }
+
+
+relatorio_disciplinascomaluno
