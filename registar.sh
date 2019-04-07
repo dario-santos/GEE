f9ce6e2caa6a99114dfcf6e61bd23882e25feedf
dario-santos <dariovfsantos@gmail.com>
2019-04-07 18:02:40 +0100

    Possivel registo final
    Agora no registo de disciplinas e alunos, apenas mostra os professores possiveis

--- registar.sh
+++ registar.sh
@@ -5,202 +5,204 @@ function new_id {
     return $maxid
 }
 function registar_universidade {
-    fO="./BD/UniOrig.txt"
-    fD="./BD/UniDest.txt"
-    echo -e "\n--Registar Universidade--\n"
+    fUniOrig="./BD/UniOrig.txt"
+    fUniDest="./BD/UniDest.txt"
+
     while :
     do
-        echo "\n Universidade: \n"
+        echo -e "\n--Registar Universidade--\n"
         echo -e "\t1 - Origem"
         echo -e "\t2 - Destino"
+        echo -n "Introduza a opção: "
         read opUni
         
         case $opUni in
         "1")
-            new_id $fO
+            new_id $fUniOrig
             maxid=$?
             
             echo -n "Introduza o nome da Universidade: "
             while :
             do 
                 read nomeUni                 
+
                 #O nome já existe?
-                isPossible=$( cut -d: -f2 $fO | grep $nomeUni)
-                                
+                isPossible=$(cut -d: -f2 $fUniOrig | grep $nomeUni)
+                
                 if [ -z "$isPossible" ]; then
-                    echo "O nome da Universidade já existe! Introduza outro:"
+                    echo "$maxid:$nomeUni" >> $fUniOrig
+                    return
                 else
-                    echo "$maxid:$nomeUni" >> $fO
-                    break
+                    echo -n "O nome da Universidade já existe! Introduza outro: "
                 fi
             done
-            break ;;    
+            ;;    
         "2")
-            new_id $fD
+            new_id $fUniDest
             maxid=$?
             
             echo -n "Introduza o nome da Universidade: "
             while :
             do 
-                read nomeUni2                       
+                read nomeUniDest     
+
                 #O nome já existe?
-                isPossible=$( cut -d: -f2 $fD | grep $nomeUni2)
+                isPossible=$( cut -d: -f2 $fUniDest | grep $nomeUniDest)
                                 
                 if [ -z "$isPossible" ]; then
-                    echo "O nome da Universidade já existe! Introduza outro:"
+                    echo "$maxid:$nomeUniDest" >> $fUniDest
+                    return
                 else
-                    echo "$maxid:$nomeUni2" >> $fD
-                    break
+                    echo -n "O nome da Universidade já existe! Introduza outro:"
                 fi
             done
-            break ;;
+            ;;
         *)
             echo -e "\n\tOpção inválida!\n"
             ;;
         esac
     done
 }
+
 function registar_professor {
-    fO="./BD/RespOrig.txt"
-    fD="./BD/RespDest.txt"
-    funiO="./BD/UniOrig.txt"
-    funiD="./BD/UniDest.txt"
+    fUniOrig="./BD/UniOrig.txt"
+    fUniDest="./BD/UniDest.txt"
+
+    fRespOrig="./BD/RespOrig.txt"
+    fRespDest="./BD/RespDest.txt"
     echo -e "\n--Registar Professor--\n"
     while :
     do
-        echo "\n Escolha o local de onde é o Professor: \n"
+        echo -e "Escolha o local de onde é o Professor: \n"
         echo -e "\t1 - Origem"
         echo -e "\t2 - Destino"
+        echo -n "Introduza a opção: "
         read opProf
         
         case $opProf in
-        "1")
-            new_id $fO
-            maxid=$?
+            "1")
+                new_id $fRespOrig
+                maxid=$?
             
-            echo -n "Introduza o nome do Professor: "
-            read nomeProf
+                echo -n "Introduza o nome do Professor: "
+                read nomeProf
             
-            cat $funiO
-            while :
-            do 
-                echo -en "\nEscolha o id da Universidade do Professor: "
-                read idUniOrig           
-                #O id é possível?
-                isPossible=$( cut -d: -f1 $funiO | grep $idUniOrig)
-                if [ -z "$isPossible" ]; then
-                    echo "O id introduzido está invalido, introduza outro!"
-                else
-                    break
-                fi
-            done
+                cat $fUniOrig | cut -d: -f1,2
+                while :
+                do 
+                    echo -en "\nEscolha o id da Universidade do Professor: "
+                    read idUniOrig           
+
+                    #O id é possível?
+                    isPossible=$( cut -d: -f1 $fUniOrig | grep $idUniOrig)
+
+                    if [ -z "$isPossible" ]; then
+                        echo "O id introduzido está invalido, introduza outro!"
+                    else
+                        echo "$maxid:$nomeProf:$idUniOrig" >> $fRespOrig
+                        return
+                    fi
+
+                done
+                ;;    
+            "2")
+                new_id $fRespDest
+                maxid=$?
             
-            echo "$maxid:$nomeProf:$idUniOrig" >> $fO
-            break ;;    
-        "2")
-            new_id $fD
-            maxid=$?
+                echo -n "Introduza o nome do Professor: "
+                read nomeProf
             
-            echo -n "Introduza o nome do Professor: "
-            read nomeProf
-            
-            cat $funiD
-            while :
-            do 
-                echo -en "\nEscolha o id da Universidade do Professor: "
-                read idUniDest         
-                #O id é possível?
-                isPossible=$( cut -d: -f1 $funiD | grep $idUniDest)
-                if [ -z "$isPossible" ]; then
-                    echo "O id introduzido está inválido, introduza outro!"
-                else
-                    break
-                fi
-            done
-            
-            echo "$maxid:$nomeProf:$" >> $fD
-            break ;;
-        *)
-            echo -e "\n\tOpção inválida!\n" ;;
+                cat $fUniDest | cut -d: -f1,2
+                while :
+                do 
+                    echo -en "\nEscolha o id da Universidade do Professor: "
+                    read idUniDest   
+
+                    #O id é possível?
+                    isPossible=$(cut -d: -f1 $fUniDest | grep $idUniDest)
+
+                    if [ -z "$isPossible" ]; then
+                        echo "O id introduzido está inválido, introduza outro!"
+                    else
+                        echo "$maxid:$nomeProf:$idUniDest" >> $fRespDest
+                        return
+                    fi
+                done
+                ;;
+            *)
+                echo -e "\n\tOpção inválida!\n" ;;
         esac
     done
 }
+
 function registar_disciplina {
-    f="./BD/Disciplina.txt"
+
+    fDisciplina="./BD/Disciplina.txt"
+    fUniDest="./BD/UniDest.txt"
+    fRespDest="./BD/RespDest.txt"
+
     echo -e "\n--Registar Disciplina--\n"
+    
     #nome
     echo -n "Introduza o nome: "
     read nomeDisciplina
+    
     #ano
-    echo -n "Escolha entre as opções o ano: "
+    echo "Escolha entre as opções o ano: "
     while :
     do
         echo -e "\t1 - 1º Ano"
         echo -e "\t2 - 2º Ano"
         echo -e "\t3 - 3º Ano"
+        echo -n "Inserido: "
         read opAno
         
         case $opAno in
         "1")
-            ano=opAno 
+            ano=$opAno 
             break ;;
         "2")
-            ano=opAno
+            ano=$opAno
             break ;;
         "3")
-            ano=opAno
+            ano=$opAno
             break ;;
         *)
             echo -e "\n\tOpção inválida!\n" ;;
         esac
     done
+    
     #semestre
-    echo -n "Escolha entre as opções o semestre: "
+    echo "Escolha entre as opções o semestre: "
     while :
     do
         echo -e "\t1 - 1º Semestre"
         echo -e "\t2 - 2º Semestre"
+        echo -n "Inserido: "
         read opSemestre
         
         case $opSemestre in
         "1")
-            semestre=opSemestre
+            semestre=$opSemestre
             break ;;
         "2")
-            semestre=opSemestre 
+            semestre=$opSemestre 
             break ;;
         *)
             echo -e "\n\tOpção inválida!\n" ;;
         esac
     done
-    #idRespDest
-    echo -n "Introduza o id do Professor Responsável: "
-    read idRegente
     
-    fResp="./BD/RespDest.txt"
-    cat $fResp
-    while :
-    do 
-        echo -en "\nEscolha o id da Universidade do Professor: "
-        read idRespDest         
-        isPossible=$( cut -d: -f1 $fResp | grep $idRespDest)
-        if [ -z "$isPossible" ]; then
-            echo "O id introduzido está inválido, introduza outro!"
-        else
-            break
-        fi
-    done
     #idUniDest
-    echo -n "Escolha entre as opções a Universidade: "
-    fUni="./BD/UniDest.txt"
-    
+    echo -e "\nEscolha a universidade da disciplina: "
+    cat $fUniDest | cut -d: -f1,2
     while :
     do 
-        echo -en "\nEscolha o id da universidade a alterar: "
+        echo -en "\nEscolha o id: "
         read idUniDest
                         
         #O id é possível?
-        isPossible=$( cut -d: -f1 $fUni | grep $idUniDest)
+        isPossible=$(cut -d: -f1 $fUniDest | grep $idUniDest)
                         
         if [ -z "$isPossible" ]; then
             echo "O id introduzido está inválido, introduza outro!"
@@ -208,33 +210,54 @@ function registar_disciplina {
             break
         fi
     done
+
+    #idRespDest
+    echo "Lista de Professor possíveis: "
+    grep "${idUniDest}$" $fRespDest | cut -d: -f1,2 
     
-    new_id $f
+    while :
+    do 
+        echo -en "\nEscolha o id do responsavel pela disciplina: "
+        read idRespDest         
+        
+        isPossible=$( cut -d: -f1 $fRespDest | grep $idRespDest)
+
+        if [ -z "$isPossible" ]; then
+            echo "O id introduzido está inválido, introduza outro!"
+        else
+            break
+    fi
+    done
+    
+    new_id $fDisciplina
     maxid=$?
     
-    echo "$maxid:$nomeDisciplina:$ano:$semestre:$idRespDest:$idUniDest" >> $f
+    echo "$maxid:$nomeDisciplina:$ano:$semestre:$idRespDest:$idUniDest" >> $fDisciplina
 }
+
 function registar_aluno {
-    f="./BD/Aluno.txt"
-    funiD="./BD/UniDest.txt"
-    funiO="./BD/UniOrig.txt"
-    fO="./BD/RespOrig.txt"
-    fD="./BD/RespDest.txt"
+    fAluno="./BD/Aluno.txt"
+    fUniDest="./BD/UniDest.txt"
+    fUniOrig="./BD/UniOrig.txt"
+    fRespOrig="./BD/RespOrig.txt"
+    fRespDest="./BD/RespDest.txt"
     
-    new_id $f
+    new_id $fAluno
     maxid=$?
     
     echo -e "\n--Registar Aluno--\n"
     echo -n "Introduza o nome do aluno: "
     read nomeAluno
     
-    echo -n "Introduza o id da Universidade de Destino do aluno: "
-    cat $funiD
+    echo "Lista de universidades de destino: "
+    cat $fUniDest | cut -d: -f1,2
     while :
-    do 
-        read idUniDest                 
+    do
+        echo -n "Introduza o id da Universidade de Destino do aluno: " 
+        read idUniDest           
+
         #O id já existe?
-        isPossible=$( cut -d: -f1 $funiD | grep $idUniDest)
+        isPossible=$( cut -d: -f1 $fUniDest | grep $idUniDest)
         
         if [ -z "$isPossible" ]; then
             echo "O id da Universidade não existe! Introduza outro:"
@@ -243,13 +266,15 @@ function registar_aluno {
         fi
     done
       
-    echo -n "Introduza o id da Universidade de Origem do aluno: "
-    cat $funiO
+    echo "Lista de universidades de destino: "
+    cat $fUniOrig | cut -d: -f1,2
     while :
-    do 
+    do
+      
+        echo -n "Introduza o id da Universidade de Origem: " 
         read idUniOrig                 
         #O id já existe?
-        isPossible=$( cut -d: -f1 $funiO | grep $idUniOrig)
+        isPossible=$( cut -d: -f1 $fUniOrig | grep $idUniOrig)
                         
         if [ -z "$isPossible" ]; then
             echo "O id da Universidade não existe! Introduza outro:"
@@ -258,36 +283,41 @@ function registar_aluno {
         fi
     done
     
-    echo -n "Introduza o id do Professor Responsável de Destino do aluno: "
-    cat $fD
+    #idRespDest
+    echo "Lista de Professor de Destino possíveis: "
+    grep "${idUniDest}$" $fRespDest | cut -d: -f1,2 
+    
     while :
     do 
-        read idRespDest                 
-        #O id já existe?
-        isPossible=$( cut -d: -f1 $fD | grep $idUniOrig)
-                        
+        echo -en "\nEscolha o id do responsavel de destino: "
+        read idRespDest         
+        
+        isPossible=$( cut -d: -f1 $fRespDest | grep $idRespDest)
+
         if [ -z "$isPossible" ]; then
-            echo "O id da Universidade não existe! Introduza outro:"
+            echo "O id introduzido está inválido, introduza outro!"
         else
             break
-        fi
+    fi
     done
     
-    echo -n "Introduza o id do Professor Responsável de Origem do aluno: "
-    cat $fO
+    #idRespOrig
+    echo "Lista de Professor de Origem possíveis: "
+    grep "${idUniOrig}$" $fRespOrig | cut -d: -f1,2 
+    
     while :
     do 
-        read idRespOrig                 
-        #O id já existe?
-        isPossible=$( cut -d: -f1 $fO | grep $idUniOrig)
-                        
+        echo -en "\nEscolha o id do responsavel de origem: "
+        read idRespOrig
+        
+        isPossible=$( cut -d: -f1 $fRespOrig | grep $idRespOrig)
+
         if [ -z "$isPossible" ]; then
-            echo "O id da Universidade não existe! Introduza outro:"
+            echo "O id introduzido está inválido, introduza outro!"
         else
             break
-        fi
+    fi
     done
-    
-    
-    echo "$maxid:$nomeAluno:$idUniDest:$idUniOrig:$idRespDest:$idRespOrig" >> $f
+
+    echo "$maxid:$nomeAluno:$idUniDest:$idUniOrig:$idRespDest:$idRespOrig" >> $fAluno
 }
